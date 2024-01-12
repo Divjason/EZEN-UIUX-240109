f9c7cc9d8d6e9fdbaa36778af1b9a871c1a3d1db
Divjason <sejinpark@tcocs.com>
2024-01-12 17:43:01 +0900

    240112

---
+++ 240112/script.js
@@ -0,0 +1,20 @@
+const foodmenu = [
+  {
+    id: 1,
+    name: "kimbab",
+    price: 3500,
+    spicy: ["hot", "middle", "dry"],
+  },
+  {
+    id: 2,
+    name: "ramen",
+    price: 4500,
+    spicy: ["hot", "middle", "dry"],
+  },
+  {
+    id: 3,
+    name: "ddok",
+    price: 5000,
+    spicy: ["hot", "middle", "dry"],
+  },
+];
