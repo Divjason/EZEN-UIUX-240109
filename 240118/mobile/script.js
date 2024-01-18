265de467a8dd5d135d173202e50e7aa5661edcbe
Divjason <sejinpark@tcocs.com>
2024-01-18 17:19:45 +0900

    240118

---
+++ 240118/mobile/script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".right");
+console.log(btn);
+
+btn.addEventListener("click", () => {
+  alert("오늘은 즐거운 목요일!");
+});
---
+++ 240118/roblox/script.js
@@ -0,0 +1,6 @@
+const genderBtn = document.querySelector("#female");
+console.log(genderBtn);
+genderBtn.addEventListener("click", (e) => {
+  e.preventDefault;
+  alert("여성입니다.");
+});
